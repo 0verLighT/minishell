56626681224e66efd9a8e3e783fa289eee5987a3
0verLighT <alexandre@0verlight.com>
2026-01-03 16:37:12 +0100

    feat(acions): adding norminette 42 actions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/01 02:33:26 by amartel           #+#    #+#             */
-/*   Updated: 2026/01/01 02:35:17 by amartel          ###   ########.fr       */
+/*   Updated: 2026/01/03 16:35:29 by amartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,4 @@
 # include <unistd.h>
 # include "../libft/includes/libft.h"
 
-# endif
+#endif
