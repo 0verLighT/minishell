838f6a563f8369f108ee86bc4df6291d69e1ba9b
jdessoli <julien.dessoliers@hotmail.com>
2026-02-16 00:43:48 +0100

    chore: parser.h

---
+++ src/parser/parser.h
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdessoli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/15 22:41:12 by jdessoli          #+#    #+#             */
+/*   Updated: 2026/02/16 00:43:29 by jdessoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//CMD is for a command, like ls. Everything else is self explainatory
+typedef enum	s_node_type
+{
+	NODE_CMD,
+	NODE_PIPE,
+	NODE_AND,
+	NODE_OR,
+}	t_node_type;
+
+//Type is for the type of redirection, 
+//0: <, 1: >, 2: >>, 3: <<
+//file points to either a file or a heredoc
+typedef struct s_redirect
+{
+	size_t	type;
+	char	*file;
+	struct	s_redirect *next;
+}	t_redirect;
+
+//This is for NODE_CMD, input[0] is for the cmd
+//input[1] and more are for args
+typedef struct s_cmd_data
+{
+	char 		**input;
+	t_redirect	*redirects;
+}	t_cmd_data;
+
+//We declare t_ast_node here, in order to use it for what's next
+typedef struct	s_ast_node	t_ast_node;
+
+//Used for pipe, and, or. Pair left and right token
+typedef	struct	s_pair_data
+{
+	t_ast_node	*left;
+	t_ast_node	*right;
+}	t_pair_data;
+
+//Used to hold command or pipes, and, or
+typedef union	u_node_data
+{
+	t_cmd_data	cmd;
+	t_pair_data	pair;
+}	t_node_data;
+
+//Type idenfifies which union member is active
+//Data is for what it contains
+struct	s_ast_node
+{
+	t_node_type	type;
+	t_node_data	data;
+}
