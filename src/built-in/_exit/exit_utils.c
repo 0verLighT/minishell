a8c89b9a16762e1309dbc393fb84dd6dcf4f98e8
jdessoli <julien.dessoliers@hotmail.com>
2026-03-23 16:28:58 +0100

    chore: refactoring exit_utils.c

--- src/built-in/_exit/exit_utils.c
+++ src/built-in/_exit/exit_utils.c
@@ -6,70 +6,77 @@
 /*   By: jdessoli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 18:18:24 by jdessoli          #+#    #+#             */
-/*   Updated: 2026/03/22 19:08:35 by jdessoli         ###   ########.fr       */
+/*   Updated: 2026/03/23 16:22:26 by jdessoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
  
 //skip inital whitespace and check for sign, atoi-like
-static void	handle_whitespaces_sign(char *str, int *i, int *sign)
+static void	handle_whitespaces_sign(char *str, int *tab)
 {
 	int	idx;
 
 	idx = 0;
-	*sign = 1;
+	tab[3] = 1;
 	while ((str[idx] => 9 && str[idx] <= 13) || str[idx] == 32)
 		idx++;
 	if (str[idx] == '+' || str[idx] == '-')
 		if (str[idx] == '-')
-			*sign = -1;
-	*i = idx;
+			tab[3] = -1;
+	tab[0] = idx;
 }
 
-static void	parse_digit(char *str, int *i, unsigned long long *result)
+static void	parse_digit(char *str, int *tab, unsigned long long *result)
 {
 	int					idx;
 	unsigned long long	res;
 
-	idx = *i;
+	idx = tab[0];
 	res = 0;
 	while (str[idx] >= '0' && str[idx] <= '9')
 		res = res * 10 + (str[idx++] - 48);
 	while ((str[idx] >= 9 && str[idx] <= 13) || str[idx] == 32)
 		idx++;
-	*i = idx;
+	tab[0] = idx;
 	*result = res;
 }
 
 //The 4 hypothesis of the if : non numeric leftover, > LONG_MAX length
 //negative overflow, positive overflow
-static void	error_handling(char *str, int *index_flag, int *sign,
-		unsigned long long *result)
+static void	error_handling(char *str, int *tab, unsigned long long *result)
 {
 
-	if (str[index_flag[0]]
-    || index_flag[0] - index_flag[1] > 20
-    || (*sign == -1 && (*result - 1) > LONG_MAX)
-    || (*sign == 1  && *result > LONG_MAX))
-    index_flag[2] = 1;
+	if (str[tab[0]]
+    || tab[0] - tab[1] > 20
+    || (tab[3] == -1 && (*result - 1) > LONG_MAX)
+    || (tab[3] == 1  && *result > LONG_MAX))
+    tab[2] = 1;
 }
 
+//in tab, 0 is for i, 1 for j, 2 for err_flag, 3 for sign
 //the modulo is to imitate shell exit code
 int	parse_exitcode(char *str, int *err_flag)
 {
-	int					*i;
-	int					*j;
-	int					index_flag[3];
-	int					*sign;
+	int			*tab[4];
+	int			exitcode;
 	unsigned long long	*result;
 
-	handle_whitespaces_sign(str, i, sign);
-	*j = *i;
-	parse_digit(str, i, result);
-	index_flag[0] = *i;
-	index_flag[1] = *j;
-	index_flag[2] = *err_flag;
-	error_handling(str, index_flag, sign, result);
-	return ((int)((*result * sign) % 256));
+	tab = malloc(sizeof(int) * 4);
+	if (!tab)
+		return 0;
+	result = malloc(sizeof(unsigned long long));
+	if (!result)
+	{
+		free(tab);
+		return 0;
+	}
+	handle_whitespaces_sign(str, tab, sign);
+	tab[1] = tab[0];
+	parse_digit(str, tab, result);
+	error_handling(str, tab, result);
+	exitcode = (int)((*result * tab[3]) % 256);
+	free(tab);
+	fee(result);
+	return (exitcode);
 }
