1edb7bedf38484c0dbd294e28257ed17798d5331
0verLighT <alexandre@0verlight.com>
2026-04-07 20:13:26 +0200

    refactor: change ft_cd to remove deps on tokenizer.h

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -1,18 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 09:46:31 by jdessoli          #+#    #+#             */
-/*   Updated: 2026/04/04 19:47:39 by amartel          ###   ########.fr       */
+/*   Updated: 2026/04/07 20:10:02 by amartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "builtin.h"
-#include "tokenizer.h"
 
 /**
  * @brief after a cd, save old PWD, set cwd (current working directory)
@@ -57,60 +55,37 @@ static int	get_cwd(char **old_cwd)
 }
 
 /**
- * @brief wrap resolve_target to return an error
- * if it's a cd had no target situation
- * @details if needed, check cd_helper's last function to better understand
- * @param tokens
- * @param ctx
- * @param target
- * @param tab tab [0] correspond to token_count, tab[1] correspond to must free
- */
-static int	get_target(t_token *tokens, t_ctx *ctx, char **target, int *tab)
-{
-	*target = resolve_target(tokens, tab[0], ctx, &tab[1]);
-	if (!*target)
-		return (1);
-	return (0);
-}
-
-/**
  * @brief wrap chdir to both handles errors and free what must be on failure
  * @param target
  * @param old_cwd
  * @param tab
  */
-static int	change_dir(char *target, char *old_cwd, int *tab)
+static int	change_dir(char *target, char *old_cwd)
 {
 	if (chdir(target) != 0)
 	{
 		builtin_error("cd", target, strerror(errno));
-		if (tab[1])
-			free(target);
 		free(old_cwd);
 		return (1);
 	}
-	if (tab[1])
-		free(target);
 	return (0);
 }
 
-int	ft_cd(t_token *tokens, int token_count, t_ctx *ctx)
+int	ft_cd(char **argv, t_ctx *ctx)
 {
 	char	*old_cwd;
 	char	*target;
-	int		tab[2];
 	int		update_flag;
 
-	tab[0] = token_count;
-	tab[1] = 0;
 	if (get_cwd(&old_cwd))
 		return (1);
-	if (get_target(tokens, ctx, &target, tab))
+	target = resolve_target(argv, ctx);
+	if (!target)
 	{
 		free(old_cwd);
 		return (1);
 	}
-	if (change_dir(target, old_cwd, tab))
+	if (change_dir(target, old_cwd))
 		return (1);
 	update_flag = update_env(ctx, old_cwd);
 	return (update_flag);
